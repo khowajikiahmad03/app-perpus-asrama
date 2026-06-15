c45344c4b307328fa7feb78cb9a44483a75f8302
khowajiki <khowajikiahmad03@gmail.com>
2026-06-15 14:51:24 +0700

    ubah konfig server.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const db         = require('./koneksi');
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
 
 // ========================
 //   LOGIN & REGISTER
